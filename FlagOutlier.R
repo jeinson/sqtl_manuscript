f534d9ae4cef2a11781fbc5d1f689fb3e28bf14b
Mariia Minaeva <61414105+marie-minaeva@users.noreply.github.com>
2022-10-07 15:26:57 +0200

    Delete FlagOutlier.R

--- FlagOutlier.R
+++
@@ -1,5 +0,0 @@
-flag_outliers <- function(x){
-        quants <- quantile(x, na.rm=TRUE)[c(2,4)]
-        iqr <- IQR(x, na.rm = T)
-        return((x > quants[2] + 1.5 * iqr) | (x < quants[1] - 1.5 * iqr))
-}
